aefc76b177bbf790e9e190492063ca71923e720e
mtsonkova <mtsonkova950@gmail.com>
2024-05-05 23:48:56 +0300

    Tests for Pets API completed

--- src/test/java/PetsEndpoints.java
+++ src/test/java/PetsEndpoints.java
@@ -1,16 +1,14 @@
 import Payloads.PetsPayloads;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ReusableMethods;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-
-
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
 
 public class PetsEndpoints extends BaseAPITest {
 //TODO string urls and tags are displayed as a code instead of text
@@ -73,7 +71,59 @@ public class PetsEndpoints extends BaseAPITest {
                 .when().get("/pet/findByStatus")
                 .then().assertThat().statusCode(200).extract().response().asPrettyString();
         ReusableMethods.createFileNoAppend("availablePets.txt", response);
+
+        JsonPath jsonPath = ReusableMethods.convertStringToJSON(response);
+        int size = jsonPath.getList("").size();
+        Assert.assertTrue(size > 0);
      }
+
+     //Test is supposed to fail with error code 400 Invalid status value
+    // Instead it returns 200 OK with empty array
+     @Test
+    public void findPetByInvalidStatusValueShouldReturn400() {
+         String response = given().queryParam("status", "purchased")
+                 .when().get("/pet/findByStatus")
+                 .then().log().all().assertThat().statusCode(400).extract().response().asPrettyString();
+     }
+
+    @Test
+    public void findPetById() {
+        String response = given().pathParam("petId", "455")
+                .when().get("/pet/{petId}")
+                .then().log().all().assertThat().statusCode(200).extract().response().asPrettyString();
+    }
+
+    @Test
+    public void findPetByIncorrectId() {
+        String response = given().pathParam("petId", "-25")
+                .when().get("/pet/{petId}")
+                .then().log().all().assertThat().statusCode(404).extract().response().asPrettyString();
+
+        Assert.assertTrue(response.contains("Pet not found"));
+    }
+
+    @Test
+    public void updatePetByFormData() {
+        String response = given().header("accept", "application/json")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .pathParam("petId", 455)
+                .formParam("name", "GoldieLocks")
+                .formParam("status", "sold")
+                .when().post("/pet/{petId}")
+                .then().log().all().assertThat().statusCode(200).extract().response().asPrettyString();
+     }
+
+     @Test
+    public void deletePetByIdValidIdProvided() {
+        Response response = given().pathParam("petId", 455)
+                .when().delete("/pet/{petId}");
+        int statusCode = response.getStatusCode();
+        String message = response.getStatusLine();
+        Assert.assertTrue(statusCode == 404);
+        Assert.assertTrue(message.contains("Not Found"));
+
+     }
+
 
 
 
