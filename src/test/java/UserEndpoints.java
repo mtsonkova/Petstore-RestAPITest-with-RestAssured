bd9bc0d82fd73792a912a5c781369fe3c5d4522e
mtsonkova <mtsonkova950@gmail.com>
2024-05-07 23:53:18 +0300

    In progress

--- src/test/java/UserEndpoints.java
+++ src/test/java/UserEndpoints.java
@@ -2,6 +2,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ReusableMethods;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,25 +11,25 @@ import java.nio.file.Paths;
 
 import static io.restassured.RestAssured.*;
 
-public class UserEndpoints extends BaseAPITest{
+public class UserEndpoints extends BaseAPITest {
 
     @Test
     public void createMultipleUsersWithJsonFile() throws IOException {
-        // Specify the path to your JSON file
-        String jsonFilePath = System.getProperty("user.dir") + "/src/test/java/utilities/userDetails.json";
 
-        // Read the content of the JSON file as a string
-        String jsonBody = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
-
-        Response response = given()
-                .contentType("application/json")
-                .body(jsonBody)
-                .when()
-                .post("/user/createWithList");
+        Response response = ReusableMethods.createMultipleUserProfilesWithJsonFile();
 
         int statusCode = response.getStatusCode();
         Assert.assertTrue(statusCode == 200);
     }
 
+    @Test
+    public void getUserByUserName() throws IOException {
+        Response newUsers = ReusableMethods.createMultipleUserProfilesWithJsonFile();
+
+       String response = given().pathParam("username", "jsmith")
+                .when().get("/user/{username}")
+                .then().log().all().assertThat().statusCode(200).extract().response().asPrettyString();
+
+    }
 
 }
--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,9 +1,14 @@
 package utilities;
 
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
 
 public class ReusableMethods {
 
@@ -22,5 +27,19 @@ public class ReusableMethods {
     public static JsonPath convertStringToJSON(String text) {
         JsonPath jsonPath = new JsonPath(text);
         return jsonPath;
+    }
+
+    public static Response createMultipleUserProfilesWithJsonFile () throws IOException {
+        String jsonFilePath = System.getProperty("user.dir") + "/src/test/java/utilities/userDetails.json";
+
+        // Read the content of the JSON file as a string
+        String jsonBody = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+        Response response = given()
+                .contentType("application/json")
+                .body(jsonBody)
+                .when()
+                .post("/user/createWithList");
+        return response;
     }
 }
