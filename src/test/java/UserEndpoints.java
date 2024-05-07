c57da8831a7ab488a6f154863b264a0491fd5a0a
mtsonkova <mtsonkova950@gmail.com>
2024-05-07 23:25:08 +0300

    Started testing for UserEndpoints class

--- src/test/java/UserEndpoints.java
+++ src/test/java/UserEndpoints.java
@@ -1,3 +1,34 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.*;
+
 public class UserEndpoints extends BaseAPITest{
+
+    @Test
+    public void createMultipleUsersWithJsonFile() throws IOException {
+        // Specify the path to your JSON file
+        String jsonFilePath = System.getProperty("user.dir") + "/src/test/java/utilities/userDetails.json";
+
+        // Read the content of the JSON file as a string
+        String jsonBody = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
+
+        Response response = given()
+                .contentType("application/json")
+                .body(jsonBody)
+                .when()
+                .post("/user/createWithList");
+
+        int statusCode = response.getStatusCode();
+        Assert.assertTrue(statusCode == 200);
+    }
+
 
 }
