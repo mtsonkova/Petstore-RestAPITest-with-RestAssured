5179fa6930ea9f297d0247ad99cd639e165782bf
mtsonkova <mtsonkova950@gmail.com>
2024-10-23 15:04:44 +0300

    UsersPayload modified

--- src/test/java/Payloads/UsersPayload.java
+++ src/test/java/Payloads/UsersPayload.java
@@ -1,18 +1,23 @@
 package Payloads;
 
+import com.google.gson.JsonObject;
+
 public class UsersPayload {
     public static String createNewUser(String userName, String firstName,
                                        String lastName, String email, String password,
                                        String phone, int userStatus) {
-        return "{\n" +
-                "  \"id\": 0,\n" +
-                "  \"username\": \"" + userName + "\",\n" +
-                "  \"firstName\": \"" + firstName + "\",\n" +
-                "  \"lastName\": \"" + lastName + "\",\n" +
-                "  \"email\": \"" + email + "\",\n" +
-                "  \"password\": \"" + password + "\",\n" +
-                "  \"phone\": \"" + phone + "\",\n" +
-                "  \"userStatus\": " + userStatus + "\n" +
-                "}";
+
+        JsonObject user = new JsonObject();
+
+        user.addProperty("id", 0);
+        user.addProperty("username", userName);
+        user.addProperty("firstName", firstName);
+        user.addProperty("lastName", lastName);
+        user.addProperty("email", email);
+        user.addProperty("password", password);
+        user.addProperty("phone", phone);
+        user.addProperty("userStatus", userStatus);
+
+        return  user.toString();
     }
 }
